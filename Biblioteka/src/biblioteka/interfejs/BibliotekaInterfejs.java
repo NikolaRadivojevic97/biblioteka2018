93a68e72287dff64866427e9ee50e414db9c58c5
NikolaRadivojevic97 <nikolarad97@gmail.com>
2018-03-02 15:24:36 +0100

    uveden je interfejs bibliotekainterfejs

---
+++ Biblioteka/src/biblioteka/interfejs/BibliotekaInterfejs.java
@@ -0,0 +1,14 @@
+package biblioteka.interfejs;
+
+import java.util.LinkedList;
+
+import biblioteka.Autor;
+import biblioteka.Knjiga;
+
+public interface BibliotekaInterfejs {
+	
+	public void dodajKnjigu(Knjiga knjiga);
+	public void obrisisKnjigu(Knjiga knjiga);
+	public LinkedList<Knjiga> vratiSveKnjige();
+	public LinkedList<Knjiga> pronadjiKnjigu(String naslov, Autor autor, String isbn, String izdaavc);
+}
