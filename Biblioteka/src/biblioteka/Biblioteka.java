1aa61192c6bea5128754d4b895b0c59f2447a853
NikolaRadivojevic97 <nikolarad97@gmail.com>
2018-03-09 14:48:15 +0100

    dodata logicka kontorola na master grani

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -9,6 +9,7 @@ public class Biblioteka implements BibliotekaInterfejs{
 	private LinkedList<Knjiga> knjige=new LinkedList<Knjiga>();
 	
 	public void dodajKnjigu(Knjiga knjiga) {
+		
 		knjige.add(knjiga);
 		
 	}
@@ -28,6 +29,9 @@ public class Biblioteka implements BibliotekaInterfejs{
 
 	
 	public LinkedList<Knjiga> pronadjiKnjigu(String naslov, Autor autor, String isbn, String izdaavc) {
+		if(naslov==null) {
+			return null;
+		}
 		LinkedList<Knjiga> novaLista=new LinkedList<Knjiga>();
 		for(int i=0;i<knjige.size();i++) {
 			if(knjige.get(i).getNaslov().contains(naslov))		//da sadrzi naslov unet zbog greski
