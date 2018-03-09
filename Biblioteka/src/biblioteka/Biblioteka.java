b4852d4c6630bc772e86082ea2fab8d25a6a5d5b
NikolaRadivojevic97 <nikolarad97@gmail.com>
2018-03-09 14:38:53 +0100

    implementirana klasa biblioteka

---
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -0,0 +1,42 @@
+package biblioteka;
+
+import java.util.LinkedList;
+
+import biblioteka.interfejs.BibliotekaInterfejs;
+
+public class Biblioteka implements BibliotekaInterfejs{
+
+	private LinkedList<Knjiga> knjige=new LinkedList<Knjiga>();
+	
+	public void dodajKnjigu(Knjiga knjiga) {
+		knjige.add(knjiga);
+		
+	}
+
+	
+	public void obrisisKnjigu(Knjiga knjiga) {
+		knjige.remove(knjiga);
+		
+	}
+
+	
+	public LinkedList<Knjiga> vratiSveKnjige() {
+		return knjige;
+		
+		
+	}
+
+	
+	public LinkedList<Knjiga> pronadjiKnjigu(String naslov, Autor autor, String isbn, String izdaavc) {
+		LinkedList<Knjiga> novaLista=new LinkedList<Knjiga>();
+		for(int i=0;i<knjige.size();i++) {
+			if(knjige.get(i).getNaslov().contains(naslov))		//da sadrzi naslov unet zbog greski
+				novaLista.add(knjige.get(i));
+		}
+		
+		
+		return novaLista;
+	}
+	
+
+}
